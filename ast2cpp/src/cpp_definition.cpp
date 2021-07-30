MapleSystem/MapleFE

commit 8952e2bbb30f48ad4a10e397dfcabf3f9b1111f6

Generate objects created using object literal initializers.

ast2cpp/src/cpp_definition.cpp
@@ -77,6 +77,8 @@ std::string CppDef::EmitModuleNode(ModuleNode *node) {
 
   str += "\n\n void "s + name + "::__init_func__() { // bind \"this\" to current module\n"s;
   isInit = true;
+  emitStr = &str;
+  emitStrInsertPos = str.size();
   for (unsigned i = 0; i < node->GetTreesNum(); ++i) {
     if (auto n = node->GetTree(i)) {
       if (n->GetKind() != NK_Class)
@@ -237,22 +239,79 @@ std::string CppDef::EmitIdentifierNode(IdentifierNode *node) {
   return str;
 }
 
+// Generate vector of ObjectProp to be passed to Object constructor.
+std::string CppDef::EmitStructLiteralProps(std::string propsName, StructLiteralNode* node) {
+  std::string str;
+  str += "  std::vector<ObjectProp>"s + propsName + " = {\n"s;
+  for (unsigned i = 0; i < node->GetFieldsNum(); ++i) {
+    if (i)
+      str += ",\n"s;
+    if (auto field = node->GetField(i)) {
+      auto lit = field->GetLiteral();
+      std::string fieldName = EmitTreeNode(field->GetFieldName());
+      TypeId typId = lit->GetTypeId();
+      std::string fieldVal = EmitTreeNode(lit);
+      switch(typId) {
+        case TY_Object:
+          break;
+        case TY_Function:
+          break;
+        case TY_Boolean:
+          str += "    std::make_pair(\""s + fieldName + "\", JS_Val(bool("s + fieldVal + ")))"s;
+          break;
+        case TY_None:
+          if (fieldVal.compare("true") == 0 || fieldVal.compare("false") == 0)
+            str += "    std::make_pair(\""s + fieldName + "\", JS_Val(bool("s + fieldVal + ")))"s;
+          break;
+        case TY_Int:
+          str += "    std::make_pair(\""s + fieldName + "\", JS_Val(int64_t("s + fieldVal + ")))"s;
+          break;
+        case TY_String:
+          str += "    std::make_pair(\""s + fieldName + "\", JS_Val(new std::string("s + fieldVal + ")))"s;
+          break;
+        case TY_Number:
+        case TY_Double:
+          str += "    std::make_pair(\""s + fieldName + "\", JS_Val(double("s + fieldVal + ")))"s;
+          break;
+        case TY_Class:
+          // Handle embedded ObjectLiterals recursively
+          if (lit->GetKind() == NK_StructLiteral) {
+            std::string props = EmitStructLiteralProps(fieldName+"Props"s, static_cast<StructLiteralNode*>(lit));
+            emitStr->insert(emitStrInsertPos, props);
+            emitStrInsertPos += props.size();
+            str += "    std::make_pair(\""s + fieldName + "\", JS_Val(new Object(&Object_ctor, Object_ctor.prototype, "s + fieldName + "Props)))"s;
+          }
+          break;
+      }
+    }
+  }
+  str += "  };\n"s;
+  return str;
+}
+
 std::string CppDef::EmitDeclNode(DeclNode *node) {
   if (node == nullptr)
     return std::string();
-  std::string str;
+  std::string str, name;
   //std::string str(Emitter::GetEnumDeclProp(node->GetProp()));
   if (auto n = node->GetVar()) {
-    str += isInit ? EmitTreeNode(n) : mCppDecl.EmitTreeNode(n);
+    name += isInit ? EmitTreeNode(n) : mCppDecl.EmitTreeNode(n);
   }
   if (auto n = node->GetInit()) {
-    if (node->GetTypeId() == TY_Class)
-      str += "= &"s + n->GetName() + "_ctor"s;
+    if (node->GetTypeId() == TY_Class && node->GetInit()->GetKind() == NK_StructLiteral) {
+      // Emit obj instance with object/struct literal init
+      std::string props = EmitStructLiteralProps(name+"Props"s, static_cast<StructLiteralNode*>( node->GetInit()));
+      emitStr->insert(emitStrInsertPos, props);
+      emitStrInsertPos += props.size();
+      str += "  "s + name + " = new Object(&Object_ctor, Object_ctor.prototype, "s + name + "Props)"s;
+    }
+    else if (node->GetTypeId() == TY_Class)
+      str += name + "= &"s + n->GetName() + "_ctor"s;
     else if(n->GetKind() == NK_ArrayLiteral)
-      str += ".clear();\n"s + str + ".insert("s + str + ".end(), "s
+      str += name + ".clear();\n"s + str + ".insert("s + str + ".end(), "s
         + EmitTreeNode(n) + ")"s;
     else
-      str += " = "s + EmitTreeNode(n);
+      str += name + " = "s + EmitTreeNode(n);
   }
   str += ";\n"s;
   return str;

ast2cpp/include/cpp_declaration.h
@@ -54,5 +54,17 @@ public:
 
 };
 
+inline bool IsVarInitStructLiteral(DeclNode* node) {
+  return node->GetInit() &&
+         node->GetInit()->GetTypeId() == TY_Class &&
+         node->GetInit()->GetKind() == NK_StructLiteral;
+}
+
+inline bool IsVarInitClass(DeclNode* node) {
+  return node->GetInit() &&
+         node->GetInit()->GetTypeId() == TY_Class &&
+         node->GetInit()->GetKind() != NK_StructLiteral;
+}
+
 } // namespace maplefe
 #endif

ast2cpp/include/cpp_definition.h
@@ -27,7 +27,8 @@ public:
   Module_Handler *mHandler;
   bool            isInit;
   CppDecl        &mCppDecl;
-
+  std::string    *emitStr;
+  size_t         emitStrInsertPos;
   CppDef(Module_Handler *h, CppDecl &d) : mHandler(h), mCppDecl(d), Emitter(h->GetASTModule()) {}
 
   std::string Emit() {
@@ -58,6 +59,7 @@ public:
   virtual std::string EmitPrimArrayTypeNode(PrimArrayTypeNode *node);
   virtual std::string EmitNewNode(NewNode *node);
   virtual std::string EmitClassProps(TreeNode *node);
+  std::string EmitStructLiteralProps(std::string propsName, StructLiteralNode* node);
 };
 
 } // namespace maplefe

ast2cpp/src/cpp_declaration.cpp
@@ -156,12 +156,17 @@ std::string CppDecl::EmitDeclNode(DeclNode *node) {
     return std::string();
   std::string str;
   if (auto n = node->GetVar()) {
-    if (node->GetInit() && node->GetInit()->GetTypeId() == TY_Class) {
-      // generate "Ctor_<class> *<var> = &<class>_ctor" for TS decl "<var> = <class>"
-      str = "Ctor_"s + node->GetInit()->GetName() + " *"s +  n->GetName() + ";\n"s;
+    if (IsVarInitStructLiteral(node)) {
+      // generate obj instance decl
+      str = "Object* "s + n->GetName();
+    } else if (IsVarInitClass(node)) {
+      // generate obj instance with class constructor:
+      // C++ decl: "Ctor_<class> *<var> = &<class>_ctor" for TS decl: "<var> = <class>"
+      str = "Ctor_"s + node->GetInit()->GetName() + " *"s +  n->GetName();
     } else {
-      str += " "s + EmitTreeNode(n) + ";\n"s;
+      str += " "s + EmitTreeNode(n);
     }
+    str += ";\n"s;
   }
   return str;
 }

commit 90b4b992d8cbfb6ad099ac233379396b236ec4b1

Fix regression caused by commit 8952e2bb.

ast2cpp/src/cpp_definition.cpp
@@ -308,10 +308,12 @@ std::string CppDef::EmitDeclNode(DeclNode *node) {
     else if (node->GetTypeId() == TY_Class)
       str += name + "= &"s + n->GetName() + "_ctor"s;
     else if(n->GetKind() == NK_ArrayLiteral)
-      str += name + ".clear();\n"s + str + ".insert("s + str + ".end(), "s
+      str += name + ".clear();\n"s + name + ".insert("s + name + ".end(), "s
         + EmitTreeNode(n) + ")"s;
     else
       str += name + " = "s + EmitTreeNode(n);
+  } else {
+    str = name;
   }
   str += ";\n"s;
   return str;